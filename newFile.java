ffa0b29f95f0dfee0dc5d7df288b62ef41edb4bd
Gnane <gnanendra5x@gmail.com>
2024-03-31 06:23:51 +0530

    add new line in newFile

---
+++ newFile.java
@@ -0,0 +1,11 @@
+class NewFile {
+
+    public static void main(String args[]) {
+
+        System.out.println("newfile prints");
+
+        System.out.println("new line added and bug fix");
+
+    }
+    
+}
